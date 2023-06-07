efde3ee1b8506658d0d25cfb8d1e7aab11c1efbf
unknown <miguelmoreno142@yahoo.com>
2023-06-06 23:56:31 -0400

    building routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -116,6 +116,12 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
   try {
+  // Delete associated product tags first
+    await ProductTag.destroy({
+      where: {
+        product_id: req.params.id,
+      },
+    });
     const products = await Product.destroy({
       where: {
         id: req.params.id,
